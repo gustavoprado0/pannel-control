6823f96aa5bf958999b2b1c310444db0b6d93ff3
Gustavo Prado <gustavopsz2002@gmail.com>
2024-10-05 20:28:16 -0300

    feat:initial interface dashboard

---
+++ src/app/overview-pages/pannel-dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className="text-center">Olá Mundo</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
